f089a97246c467fe3b67c6d649b961913b3f9296
Paul Standley <paulstandley1972@gmail.com>
2020-07-07 19:43:07 +0100

    std reduce accumulate

--- LPP7.cpp
+++ LPP7.cpp
@@ -1,3 +1,8 @@
 #include "pch.h"
+#include <algorithm> // std::shuffle
+#include <array>
+#include <ctime>
 #include <iostream>
+#include <numeric> // std::reduce
+#include <random>
 #include "Header.h"
--- LessonCPP7.cpp
+++ LessonCPP7.cpp
@@ -1,12 +1,33 @@
 #include "pch.h"
+#include <algorithm> // std::shuffle
+#include <array>
+#include <ctime>
 #include <iostream>
+#include <numeric> // std::reduce
+#include <random>
 #include "Header.h"
 
-
+void std_reduce()
+{
+    std::array arr{ 1, 2, 3, 4 };
+    std::cout << std::reduce(arr.begin(), arr.end()) << '\n'; // 10
+    // If you can't use std::reduce, use std::accumulate. The 0 is the initial value
+    // of the result: 0 + (((1 + 2) + 3) + 4)
+    std::cout << std::accumulate(arr.begin(), arr.end(), 0) << '\n'; // 10
+    std::mt19937 mt{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
+    std::shuffle(arr.begin(), arr.end(), mt);
+    for (int i : arr)
+    {
+        std::cout << i << ' ';
+    }
+    std::cout << '\n';
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std_reduce();
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
