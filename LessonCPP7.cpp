c0498e642fd54c6bf719c371a22d9c5995beec7a
Paul Standley <paulstandley1972@gmail.com>
2020-07-24 22:42:47 +0100

    quiz 1 recursion

--- LessonCPP7.cpp
+++ LessonCPP7.cpp
@@ -16,11 +16,37 @@
 
 
 
+int factorial001(int num)
+{
+    if (num <= 0)
+        return 1;
+    else
+        return factorial001(num - 1) * num;
+}
+
+void recursion_quiz()
+{
+    //1) A factorial of an integer N (written N!) 
+    //is defined as the product (multiplication) 
+    //of all the numbers between 1 and N (0! = 1). 
+    //Write a recursive function called factorial
+    //that returns the factorial of the input. 
+    //Test it with the first 7 factorials.
+
+    //Hint: Remember that(x * y) = (y * x), 
+    //so the product of all the numbers between 1 and N
+    //is the same as the product of all the numbers between N and 1.
+
+    for (int count = 0; count < 7; ++count)
+        std::cout << factorial001(count) << '\n';
+    std::cout << '\n';
+
+}
 
 
 int main()
 {
-    recursion();
+    recursion_quiz();
     
 
     return 0;
