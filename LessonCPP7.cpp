df97168f5dd9a268d9bc23e1a6cb531ca898c5e2
Paul Standley <paulstandley1972@gmail.com>
2020-08-01 21:15:45 +0100

    Mutable capture by value

--- LessonCPP7.cpp
+++ LessonCPP7.cpp
@@ -65,13 +65,108 @@ void lambda_captures()
     {
         std::cout << "Found " << *found << '\n';
     }
+
+    /*So how do captures actually work?
+
+    While it might look like our lambda in the example above is directly accessing the
+    value of main‘s search variable, this is not the case.
+    Lambdas might look like nested blocks, but they work slightly differently
+    (and the distinction is important).
+
+    When a lambda definition is executed,
+    for each variable that the lambda captures, 
+    a clone of that variable is made (with an identical name) inside the lambda. 
+    These cloned variables are initialized from the outer scope variables
+    of the same name at this point.
+
+    Thus, in the above example, when the lambda object is created, 
+    the lambda gets its own cloned variable named search.
+    This cloned search has the same value as main‘s search, 
+    so it behaves like we’re accessing main‘s search, but we’re not.
+
+    While these cloned variable have the same name, 
+    they don’t necessarily have the same type as the original variable.
+    
+    Key insight
+
+    The captured variables of a lambda are clones of the outer scope variables,
+    not the actual variables.
+
+    For advanced readers
+
+    Although lambdas look like functions, 
+    they’re actually objects that can be called like functions
+    (these are called functors -- we’ll discuss how to create your own functors 
+    from scratch in a future lesson).
+    
+    When the compiler encounters a lambda definition,
+    it creates a custom object definition for the lambda.
+    Each captured variable becomes a data member of the object.
+
+    At runtime, when the lambda definition is encountered, 
+    the lambda object is instantiated, 
+    and the members of the lambda are initialized at that point.
+    
+    Captures default to const value
+
+    By default, variables are captured by const value. 
+    This means when the lambda is created, 
+    the lambda captures a constant copy of the outer scope variable, 
+    which means that the lambda is not allowed to modify them.
+    In the following example, we capture the variable ammo and try to decrement it.
+
+    */
+
+    int ammo1{ 10 };
+
+    // Define a lambda and store it in a variable called "shoot".
+    auto shoot1{
+      [ammo1]() {
+            int val{ammo1};
+            // Illegal, ammo was captured as a const copy.
+            --val;
+            //--ammo1;
+            std::cout << "Pew! " << val << " shot(s) left.\n";
+          }
+    };
+
+    // Call the lambda
+    shoot1();
+
+    std::cout << ammo1 << " shot(s) left\n";
+
+    int ammo2{ 10 };
+
+    auto shoot2{
+        // Added mutable after the parameter list.
+        [ammo2]() mutable {
+            // We're allowed to modify ammo now
+            --ammo2;
+
+            std::cout << "Pew! " << ammo2 << " shot(s) left.\n";
+          }
+    };
+
+    shoot2();
+    shoot2();
+    std::cout << ammo2 << " shot(s) left\n";
+
+    /*While this now compiles, there’s still a logic error. 
+    What happened? When the lambda was called, the lambda captured a copy of ammo. 
+    When the lambda decremented ammo from 10 to 9 to 8, 
+    it decremented its own copy, not the original value.
+
+    Note that the value of ammo is preserved across calls to the lambda!
+    
+    */
+
 }
 
 
 int main()
 {
-    lambdas_quiz();
-    lambdas_quiz1();
+    lambda_captures();
+
 
     return 0;
 }
